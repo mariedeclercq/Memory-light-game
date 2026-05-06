885aa22c45a299d811c2ad3050b7a1ed3745b983
Hanne-Dierickx <hanne.dierickx@ugent.be>
2026-05-06 21:48:07 +0200

    outputs_valideren

---
+++ arduino/inputs_outputs_valideren/outputs_valideren/outputs_valideren.ino
@@ -0,0 +1,41 @@
+int ledPinrood = 8;
+int ledPingroen = 9;
+int ledPinblauw = 10;
+int ledPingeel = 11;
+
+
+void setup() {
+  pinMode(ledPinrood, OUTPUT);
+  pinMode(ledPingroen, OUTPUT);
+  pinMode(ledPinblauw, OUTPUT);
+  pinMode(ledPingeel, OUTPUT);
+
+  Serial.begin(9600);
+}
+
+
+void loop() {
+  digitalWrite(ledPinrood, HIGH);
+  Serial.println("LED rood aan");
+  delay(1000);
+  digitalWrite(ledPinrood, LOW);
+  delay(1000);
+
+  digitalWrite(ledPingroen, HIGH);
+  Serial.println("LED groen aan");
+  delay(1000);
+  digitalWrite(ledPingroen, LOW);
+  delay(1000);
+
+  digitalWrite(ledPinblauw, HIGH);
+  Serial.println("LED blauw aan");
+  delay(1000);
+  digitalWrite(ledPinblauw, LOW);
+  delay(1000);
+
+  digitalWrite(ledPingeel, HIGH);
+  Serial.println("LED geel aan");
+  delay(1000);
+  digitalWrite(ledPingeel, LOW);
+  delay(1000);
+}
