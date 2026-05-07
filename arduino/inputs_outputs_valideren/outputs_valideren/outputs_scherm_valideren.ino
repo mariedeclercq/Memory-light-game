dcb8d4d43ea01a417bfd22bf642f10011741e550
mariedeclercq <maricle.declercq@ugent.be>
2026-05-07 21:44:00 +0200

    validatie_scherm

---
+++ arduino/inputs_outputs_valideren/outputs_valideren/outputs_scherm_valideren.ino
@@ -0,0 +1,11 @@
+// Via deze kleine test werd nagegaan of het programme met succes werkt of niet.
+
+void setup() {
+  size(400, 200);
+}
+
+void draw() {
+  background(0);
+  fill(255);
+  text("Processing werkt", 20, 50);
+}
